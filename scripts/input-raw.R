a2680f736422a45147afc7a8da6a58001986711f
Felipe Figueiredo <philsf79@gmail.com>
2022-01-24 22:17:46 -0300

    input raw - script pprincipal de tratamento dos dados

---
+++ scripts/input-raw.R
@@ -0,0 +1,64 @@
+# setup -------------------------------------------------------------------
+library(philsfmisc)
+# library(data.table)
+library(tidyverse)
+library(readxl)
+# library(haven)
+# library(foreign)
+# library(lubridate)
+library(labelled)
+
+# data loading ------------------------------------------------------------
+set.seed(42)
+data.raw <- tibble(id=gl(2, 10), group = gl(2, 10), outcome = rnorm(20))
+data.raw <- read_excel("dataset/atq septuagenarios DESCONTADOS.xlsx") %>%
+  janitor::clean_names()
+
+# data cleaning -----------------------------------------------------------
+
+data.raw <- data.raw %>%
+  # select() %>%
+  mutate(
+    has = !is.na(has),
+    dm = !is.na(dm),
+    tabagismo = !is.na(tabagismo),
+  ) %>%
+  filter()
+
+# data wrangling ----------------------------------------------------------
+
+data.raw <- data.raw %>%
+  mutate(
+    prontuario = factor(prontuario), # or as.character
+  ) %>%
+  mutate(
+    # anemia - qualquer
+    anemia = str_detect(complicacoes, regex("anemia", ignore_case = TRUE)),
+    # tep
+    tep = str_detect(complicacoes, regex("tep", ignore_case = TRUE)),
+    
+  )
+
+# labels ------------------------------------------------------------------
+
+data.raw <- data.raw %>%
+  set_variable_labels(
+    # group = "Study group",
+    # outcome = "Study outcome",
+  )
+
+# analytical dataset ------------------------------------------------------
+
+analytical <- data.raw #%>%
+  # select analytic variables
+  # select(
+    # prontuario,
+    # group,
+    # outcome,
+  # )
+
+# mockup of analytical dataset for SAP and public SAR
+analytical_mockup <- tibble( prontuario = c( "1", "2", "3", "...", as.character(nrow(analytical)) ) ) %>%
+  left_join(analytical %>% head(0), by = "prontuario") %>%
+  mutate_all(as.character) %>%
+  replace(is.na(.), "")
